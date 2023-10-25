216ab7a0e78532bfe13d58de01aa20a829b666c0
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-10-25 15:40:21 -0400

    done

---
+++ index.js
@@ -0,0 +1,56 @@
+function add() {}
+function subtract() {}
+function multiply() {}
+function divide() {}
+
+function add() {
+    return "something";
+}
+function add(a,b) {
+    return "something";
+}
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract() {
+    return "something";
+}
+function subtract(a,b) {
+    return "something";
+}
+function subtract(a,b) {
+    return a - b;
+}
+function multiply() {
+    return "something"
+}
+function multiply(a,b) {
+    return "something";
+}
+function multiply(a,b) {
+    return a * b;
+}
+function divide() {
+    return "something"
+}
+function divide(a,b) {
+    return "something";
+}
+function divide(a,b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+function decrement(n) {
+    return n - 1;
+}
+function makeInt(n) {
+return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
